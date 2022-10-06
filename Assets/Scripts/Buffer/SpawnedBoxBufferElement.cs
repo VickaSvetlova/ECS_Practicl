7667fe41c52b46c8c66e3e196a8a4546fad7448b
Tory Shepard <vickasvetlova@gmail.com>
2022-10-06 19:54:42 +0400

    sync

---
+++ Assets/Scripts/Buffer/SpawnedBoxBufferElement.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+namespace Buffer
+{
+    [InternalBufferCapacity(8)]
+    public struct SpawnedBoxBufferElement: IBufferElementData
+    {
+        public Entity Value;
+    }
+}
--- Assets/Scripts/Systems/SpawnerBoxSystem.cs
+++ Assets/Scripts/Systems/SpawnerBoxSystem.cs
@@ -1,3 +1,4 @@
+using Buffer;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -7,14 +8,20 @@ using UnityEngine;
 public partial class SpawnerBoxSystem : SystemBase
 {
     private Entity _prefabCube;
-    private EndInitializationEntityCommandBufferSystem _ecbSystem;
+    private Entity _cubeSpawner;
+    private BeginInitializationEntityCommandBufferSystem _ecbSystem;
     public int CountX = 100;
     public int CountY = 100;
 
     protected override void OnStartRunning()
     {
+        base.OnCreate();
         Application.targetFrameRate = 30;
         _prefabCube = GetSingleton<SamplePrefab>().Value;
+        _ecbSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+
+        _cubeSpawner = GetSingletonEntity<LastSpawnedCube>();
+        EntityManager.AddBuffer<SpawnedBoxBufferElement>(_cubeSpawner);
         SpawnBoxFromNoice();
     }
 
@@ -32,9 +39,12 @@ public partial class SpawnerBoxSystem : SystemBase
         {
             for (int y = 0; y < CountY; y++)
             {
-                var instance = EntityManager.Instantiate(_prefabCube);
+                var ecb = _ecbSystem.CreateCommandBuffer();
+                var instance = ecb.Instantiate(_prefabCube);
                 var position = (new float3(x * 1.3f, noise.cnoise(new float2(x, y) * 0.21f) * 2, y * 1.3f));
-                EntityManager.SetComponentData(instance, new Translation { Value = position });
+                ecb.SetComponent(instance, new Translation { Value = position });
+
+                ecb.AppendToBuffer(_cubeSpawner, new SpawnedBoxBufferElement { Value = instance });
             }
         }
     }
